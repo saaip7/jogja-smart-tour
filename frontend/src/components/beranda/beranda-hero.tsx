32c5e6af473dc3dae07ca3f71daeee0d207fcf16
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2025-03-27 21:30:57 +0700

    feat: hero section for below md size

--- frontend/src/components/beranda/beranda-hero.tsx
+++ frontend/src/components/beranda/beranda-hero.tsx
@@ -3,46 +3,97 @@ import { OwnButton } from "../OwnButton";
 
 export const BerandaHero = () => {
   return (
-    <section className="relative w-full pt-10 lg:pt-16 h-[fit-content] hidden md:block">
-      <div className=" relative mx-12">
-        <Image
-          src="/beranda/hero-base.png"
-          alt="hero"
-          style={{ objectFit: "cover" }}
-          width={3500}
-          height={0}
-          draggable={false}
-        />
-        {/* Overlay Candi */}
-        <div className="absolute bottom-0">
+    <section>
+      {/* above md */}
+      <div className="relative w-full pt-10 lg:pt-16 h-[fit-content] hidden md:block">
+        <div className=" relative mx-12">
           <Image
-            src="/beranda/hero-overlay.png"
-            alt="hero overlay"
+            src="/beranda/hero-base.png"
+            alt="hero"
+            style={{ objectFit: "cover" }}
             width={3500}
             height={0}
             draggable={false}
           />
+          {/* Overlay Candi */}
+          <div className="absolute bottom-0">
+            <Image
+              src="/beranda/hero-overlay.png"
+              alt="hero overlay"
+              width={3500}
+              height={0}
+              draggable={false}
+            />
+          </div>
+
+          {/* Overlay dark */}
+          <div
+            className="absolute w-full h-full bottom-0 opacity-80 rounded-[14px] lg:rounded-[28px]"
+            style={{
+              background:
+                "linear-gradient(60deg, #000 -7%, rgba(0, 0, 0, 0.00) 65%)",
+            }}
+          />
+
+          {/* Text */}
+          <div className="absolute inset-0 h-full flex flex-col justify-center md:p-12 lg:p-24">
+            <h1 className="md:text-2xl lg:text-4xl font-semibold mb-2 text-white">
+              Temukan Pesona <span className="text-[#7CF8FF]">Jogja</span>,{" "}
+              <br />
+              Rancang Sesuai Gaya <br />
+              Liburanmu!
+            </h1>
+            <OwnButton className="w-[fit-content] mt-2">
+              Mulai Sekarang
+            </OwnButton>
+          </div>
         </div>
+      </div>
 
-        {/* Overlay dark */}
-        <div
-          className="absolute w-full h-full bottom-0 opacity-80 rounded-[14px] lg:rounded-[28px]"
-          style={{
-            background:
-              "linear-gradient(60deg, #000 -7%, rgba(0, 0, 0, 0.00) 65%)",
-          }}
-        />
+      {/* below md */}
+      <div className="relative w-full pt-10 h-[fit-content] block md:hidden">
+        <div className=" relative mx-12">
+          <Image
+            src="/beranda/hero-base-mobile.png"
+            alt="hero"
+            style={{ objectFit: "cover" }}
+            width={400}
+            height={0}
+            draggable={false}
+          />
+          {/* Overlay Candi */}
+          <div className="absolute bottom-0">
+            <Image
+              src="/beranda/hero-overlay-mobile.png"
+              alt="hero overlay"
+              width={400}
+              height={0}
+              draggable={false}
+            />
+          </div>
 
-        {/* Text */}
-        <div className="absolute inset-0 h-full flex flex-col justify-center md:p-12 lg:p-24">
-          <h1 className="md:text-2xl lg:text-4xl font-semibold mb-2 text-white">
-            Temukan Pesona <span className="text-[#7CF8FF]">Jogja</span>, <br />
-            Rancang Sesuai Gaya <br />
-            Liburanmu!
-          </h1>
-          <OwnButton className="w-[fit-content] mt-2">
-            Mulai Sekarang
-          </OwnButton>
+          {/* Overlay dark */}
+          <div
+            className="absolute w-full h-full bottom-0 opacity-80 rounded-[24px]"
+            style={{
+              background:
+                "linear-gradient(0deg, #000 -5%, rgba(0, 0, 0, 0.00) 50%)",
+            }}
+          />
+
+          {/* Text */}
+          <div className="absolute inset-0 h-full flex flex-col justify-end items-center p-6 sm2:p-10">
+            <h1 className="text-[18px] sm2:text-[22px] font-semibold mb-2 text-white text-center">
+              Temukan Pesona <span className="text-[#7CF8FF]">Jogja</span>,{" "}
+              <br />
+              Rancang Sesuai Gaya <br />
+              Liburanmu!
+            </h1>
+            <OwnButton className="w-[fit-content] mt-2">
+              Mulai Sekarang
+            </OwnButton>
+          </div>
+
         </div>
       </div>
     </section>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+		screens: {
+			'sm2': '480px'
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
